edd05225e3550349034c5cba25f0e81c2eafdc19
Gonzalo10ab <gonzalo10ab@users.noreply.github.com>
2025-11-04 20:39:37 +0100

    Ejemplo de Crud con API

---
+++ app/Http/Controllers/Api/BandController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Band;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\BandRequest;
+use App\Http\Resources\BandResource;
+
+class BandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return BandResource::collection(Band::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(BandRequest $request)
+    {
+        $band = Band::create($request->validated());
+
+        return new BandResource($band);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Band $band)
+    {
+        return new BandResource($band);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(BandRequest $request, Band $band)
+    {
+        $band->update($request->validated());
+
+        return new BandResource($band);
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Band $band)
+    {
+        $band->delete();
+
+        return response()->json(['message' => 'Band deleted successfully']);
+    }
+}
